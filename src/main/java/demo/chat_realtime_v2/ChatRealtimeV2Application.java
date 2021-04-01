2fcbd96528db1a841ded4e38b86cec374757f38a
Phan Thế Kỷ <thekypccc@gmail.com>
2021-04-01 12:18:31 +0700

    version 01, public chat room, verify sender by input form

---
+++ src/main/java/demo/chat_realtime_v2/ChatRealtimeV2Application.java
@@ -0,0 +1,13 @@
+package demo.chat_realtime_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRealtimeV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatRealtimeV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/demo/chat_realtime_v2/ChatRealtimeV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.chat_realtime_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRealtimeV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
